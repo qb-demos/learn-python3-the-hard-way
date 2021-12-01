e574817b5b1e963f82b372ccee73c3d13ced970b
maqingbo <wmaqingbo@163.com>
2021-12-01 20:58:48 +0800

    chore;

---
+++ ex5.py
@@ -0,0 +1,11 @@
+my_name = 'qb'
+my_age = 29
+my_height = 173
+my_weight = 64
+
+print(f"let's talk about {my_name}")
+print(f"He is {my_height} tall.")
+
+ # this line is tricky, try to get it exactly right
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}.")
